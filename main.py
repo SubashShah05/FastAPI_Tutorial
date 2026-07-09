94075872485f93dc90de839d65b855b7491c5fd8
Subash Shah <suwashshah5@gmail.com>
2026-07-09 23:10:38 +0530

    Initial commit

--- main.py
+++ main.py
@@ -2,6 +2,24 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+#Home route
 @app.get("/")
 def home():
     return {"message": "Welcome to the FastAPI application!"}   
+
+#About Route
+@app.get("/about")
+def about():
+    return{"message":"This is About page of the FastAPI application!"}
+
+
+#Users Route
+@app.get("/users")
+def users():
+    return{
+        "users": [
+            {"id": 1, "name": "John Doe"},
+            {"id": 2, "name": "Jane Smith"},
+            {"id": 3, "name": "Alice Johnson"}
+        ]
+    }
